996ff4e6b5b03b6e814a551d3dfc7ebc22da9e64
Oksana-Botsieva <bocievaoks@yandex.ru>
2024-07-30 13:39:14 +0400

    layout of the PersonalInfo block is completed

---
+++ src/components/Header/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  font-size: 20px;
+  background: #1d1e2c;
+  padding: 30px 50px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
