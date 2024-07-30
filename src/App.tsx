f60c8cdcedf83daf854cb56ee98d05e97adb6bda
Oksana-Botsieva <bocievaoks@yandex.ru>
2024-07-30 16:12:14 +0400

    layout of the feature_Education block is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
 import { PersonalInfo } from "./components/PersonalInfo/PersonalInfo";
+import { Education } from "./components/Education/Education";
 
 function App() {
   return (
-      <div className="App container">
-        <Header />
-        <PersonalInfo />
-      </div>
+    <div className="App container">
+      <Header />
+      <PersonalInfo />
+      <Education />
+    </div>
   );
 }
 
