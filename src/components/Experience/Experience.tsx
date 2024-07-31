192cd48e07eb64eeef33d570135152e0d1cb6503
Oksana-Botsieva <bocievaoks@yandex.ru>
2024-07-31 14:12:22 +0400

    Work Experience block under development

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../Card/Card";
 import { Control } from "../ControlForm/Control";
-import { DatePicker, Input } from "antd";
+import { Button, DatePicker, Input } from "antd";
+import * as S from "./style";
 
-const Experience = () => {
+const Experience: React.FC = () => {
   const { RangePicker } = DatePicker;
   const { TextArea } = Input;
-  return (
-    <Card>
+  const [forms, setForms] = useState<number[]>([]);
+  const addForm = () => {
+    setForms([...forms, forms.length]);
+  };
+
+  const renderForm = (index: number) => (
+    <Card key={index}>
+      <S.ExperienceTitle>Место работы</S.ExperienceTitle>
       <Control label={"Компания"}>
         <Input />
       </Control>
       <Control label={"Должность"}>
         <Input />
       </Control>
-      <Control label={"Преиод работы"}>
+      <Control label={"Период работы"}>
         <RangePicker placeholder={["Начало", "Конец"]} />
       </Control>
       <Control label={"Описание"}>
         <TextArea rows={5} />
       </Control>
     </Card>
+  );
+  return (
+    <>
+      <Card>
+        <S.ExperienceTitle>Место работы</S.ExperienceTitle>
+        <Control label={"Компания"}>
+          <Input />
+        </Control>
+        <Control label={"Должность"}>
+          <Input />
+        </Control>
+        <Control label={"Период работы"}>
+          <RangePicker placeholder={["Начало", "Конец"]} />
+        </Control>
+        <Control label={"Описание"}>
+          <TextArea rows={5} />
+        </Control>
+      </Card>
+      {forms.map((_, index) => renderForm(index))}
+      <Button onClick={addForm} type={"primary"}>
+        Добавить ещё
+      </Button>
+    </>
   );
 };
 
