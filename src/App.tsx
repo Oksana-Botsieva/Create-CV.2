b85041bcfd093bf226e98f243ea71474f903f7ab
Oksana-Botsieva <bocievaoks@yandex.ru>
2024-08-05 16:56:51 +0400

    setting up redux for the PersonalInfo.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import { Skills } from "./components/Skills/Skills";
 import { Education } from "./components/Education/Education";
 import { Experience } from "./components/Experience/Experience";
 import { Title } from "./components/Title/Title";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App container">
-      <Header />
-      <Title title={"Персональная информация"} />
-      <PersonalInfo />
-      <Title title={"Образование"} />
-      <Skills />
-      <Title title={"Навыки"} />
-      <Education />
-      <Title title={"Опыт работы"} />
-      <Experience />
-    </div>
+    <Provider store={store}>
+      <div className="App container">
+        <Header />
+        <Title title={"Персональная информация"} />
+        <PersonalInfo />
+        <Title title={"Образование"} />
+        <Skills />
+        <Title title={"Навыки"} />
+        <Education />
+        <Title title={"Опыт работы"} />
+        <Experience />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/PersonalInfo/PersonalInfo.tsx
+++ src/components/PersonalInfo/PersonalInfo.tsx
@@ -1,78 +1,117 @@
-import React, {ChangeEvent, useState} from "react";
-import {Card} from "../Card/Card";
-import {DatePicker, Input} from "antd";
-import dayjs, {Dayjs} from "dayjs";
-import {Control} from "../ControlForm/Control";
-import {Upload} from "../Upload/Upload";
+import React, { useState } from "react";
+import { Card } from "../Card/Card";
+import { DatePicker, Input } from "antd";
+import { Dayjs } from "dayjs";
+import { Control } from "../ControlForm/Control";
+import { Upload } from "../Upload/Upload";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import {
+  setAddress,
+  setAvatar,
+  setBirthday,
+  setFio,
+  setPhone,
+} from "../../store/personalInfoSlice";
 
 const PersonalInfo: React.FC = () => {
-    const [info, setInfo] = useState({
-        name: "",
-        birthDate: "",
-        address: "",
-        phone: "",
-        avatar: null as string | null,
-    });
+  const dispatch: AppDispatch = useDispatch();
+  const personalInfo = useSelector((state: RootState) => state.personalInfo);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setInfo((prevInfo) => ({
-            ...prevInfo,
-            [name]: value,
-        }));
-    };
+  const [fio, setFioState] = useState(personalInfo.fio);
+  const [address, setAddressState] = useState(personalInfo.address);
+  const [phone, setPhoneState] = useState(personalInfo.phone);
+  const [birthday, setBirthdayState] = useState<Dayjs | null>(
+    personalInfo.birthday,
+  );
+  const [avatar, setAvatarState] = useState(personalInfo.avatar);
 
-    const handleDateChange = (date: Dayjs | null, dateString: string) => {
-        setInfo((prevInfo) => ({
-            ...prevInfo,
-            birthDate: dateString,
-        }));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "fio":
+        setFioState(value);
+        break;
+      case "address":
+        setAddressState(value);
+        break;
+      case "phone":
+        setPhoneState(value);
+        break;
+      case "avatar":
+        setAvatarState(value);
+        break;
+    }
+  };
+  const handleDateChange = (date: Dayjs | null) => {
+    setBirthdayState(date);
+  };
 
-    const handleUploadChange = (img: string) => {
-        setInfo((prevInfo) => ({
-            ...prevInfo,
-            avatar: img,
-        }));
-    };
-    return (
-        <Card>
-            <Control label={"ФИО"}>
-                <Input
-                    placeholder="ФИО"
-                    name="name"
-                    value={info.name}
-                    onChange={handleChange}
-                />
+  const handleUploadChange = (img: string) => {
+    setAvatarState(img);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(setFio(fio));
+    dispatch(setAddress(address));
+    dispatch(setPhone(phone));
+    dispatch(setBirthday(birthday));
+    dispatch(setAvatar(avatar));
+  };
 
-            </Control>
-            <Control label={"Место жительства"}>
-                <Input
-                    placeholder="Место жительства"
-                    name="address"
-                    value={info.address}
-                    onChange={handleChange}
-                />
-            </Control>
-            <Control label={"Номер Телефона"}>
-                <Input
-                    placeholder="Номер телефона"
-                    name="phone"
-                    value={info.phone}
-                    onChange={handleChange}
-                />
-            </Control>
-            <Control label={"Дата рождения"}>
-                <DatePicker
-                    value={info.birthDate ? dayjs(info.birthDate) : null}
-                    onChange={(date, dateString) =>
-                        handleDateChange(date, dateString as string)
-                    }
-                />
-            </Control>
-            <Upload onChange={handleUploadChange}></Upload>
-        </Card>
-    );
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+  //   setInfo((prevInfo) => ({
+  //     ...prevInfo,
+  //     [name]: value,
+  //   }));
+  // };
+  //
+  // const handleDateChange = (date: Dayjs | null, dateString: string) => {
+  //   setInfo((prevInfo) => ({
+  //     ...prevInfo,
+  //     birthDate: dateString,
+  //   }));
+  // };
+  //
+  // const handleUploadChange = (img: string) => {
+  //   setInfo((prevInfo) => ({
+  //     ...prevInfo,
+  //     avatar: img,
+  //   }));
+  // };
+  return (
+    <Card>
+      <Control label={"ФИО"}>
+        <Input
+          placeholder="ФИО"
+          name="name"
+          value={fio}
+          onChange={handleChange}
+        />
+      </Control>
+      <Control label={"Место жительства"}>
+        <Input
+          placeholder="Место жительства"
+          name="address"
+          value={address}
+          onChange={handleChange}
+        />
+      </Control>
+      <Control label={"Номер Телефона"}>
+        <Input
+          placeholder="Номер телефона"
+          name="phone"
+          value={phone}
+          onChange={handleChange}
+        />
+      </Control>
+      <Control label={"Дата рождения"}>
+        <DatePicker value={birthday} onChange={handleDateChange} />
+      </Control>
+      <Upload onChange={handleUploadChange}></Upload>
+    </Card>
+  );
 };
 
-export {PersonalInfo};
+export { PersonalInfo };
